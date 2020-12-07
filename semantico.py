7785dd731cc542b08c038b58db13c5a9ab6c72b8
souzalucas <lsantos.2016@alunos.utfpr.edu.br>
2020-12-07 16:50:44 -0300

    Iniciando analise semantica

---
+++ semantico.py
@@ -0,0 +1,11 @@
+from anytree import Node
+
+import anytree
+
+# Constantes
+OPERATIONS = ['+', '-', '*', '/', ':=', ':']
+NOS_PODA = [
+    'acao', 'expressao', 'expressao_logica', 'expressao_simples', 'expressao_aditiva', 
+    'expressao_multiplicativa', 'expressao_unaria', 'operador_relacional', 
+    'operador_logico', 'operador_negacao', 'fator', 'lista_variaveis'
+    ]
