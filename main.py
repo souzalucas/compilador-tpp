16d7397a9764186f634ee8e0d2a2cbd40eba37a9
souzalucas <lsantos.2016@alunos.utfpr.edu.br>
2020-12-06 21:41:08 -0300

    Atualizando sintatico

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ import os
 from anytree.exporter import UniqueDotExporter
 
 import sintatico as sin
-# import semantico as sem
-# import generate_code as gen
 
 def main():
     arquivo_teste = open(sys.argv[1], 'r', encoding = 'utf-8').read()
     numero_linhas = sum(1 for linha in open(sys.argv[1], 'r', encoding = 'utf-8'))
 
-    # Syntax
-    arvore, sintatico_sucesso = sin.parser(arquivo_teste, numero_linhas)
+    # Sintatico
+    arvore, sintatico_sucesso = sin.analisador(arquivo_teste, numero_linhas)
     
-    # Semantics
+    # Semantico
     if (sintatico_sucesso):
-        # arvore, symbol_table, sema_sucesso = sem.semantics(arvore)
-        UniqueDotExporter(arvore).to_picture("program.png")
-
-        # if(sema_success):
-        #     gen.gen_code(arvore, symbol_table, sema_success)
+        UniqueDotExporter(arvore).to_picture("programa.png")
 
 main()
